85bb5c0afdcdbfb987fb4684acb068ad1991e309
Vikram Narayan <vnaray1@gmail.com>
2014-11-12 02:32:46 -0500

    created synset thesauri; each has the name [author]_tagged.thes

--- lesk.py
+++ lesk.py
@@ -6,6 +6,8 @@ Word sense disambiguation using Lesk algorithm and part of speech tagging.
 from nltk.wsd import lesk
 from nltk.tag import pos_tag
 from nltk import word_tokenize
+import config
+import string
 
 ADJ, ADJ_SAT, ADV, NOUN, VERB = 'a', 's', 'r', 'n', 'v'
 
@@ -29,24 +31,54 @@ def ptb_to_wn_pos(ptb_tag):
     return wn_pos
 
 
-def pos_tagger(string):
+def my_lesk(tagged_string, desired_word):
     """
-    Returns:
+    @tagged_string: the string of words in the format 'word1_pos1 word2_pos2...'
+    @desired_word: the word we want disambiguated
+    Returns: - synset returned by lesk with part of speech (more accurate)
+             - synset returned by lesk without pos specified if no pos (less accurate)
+             - None if lesk returns nothing
     """
-    tagged = pos_tag(word_tokenize(string.decode('ascii','ignore')))
-    disambiguated = []
-    for word, tag in tagged:
-        # ignore proper nouns
-        if tag=='NNP' or tag=='NNPS':
-            temp=[word,tag,None]
-        else:
-            temp = [word, tag, lesk(string, word, ptb_to_wn_pos(tag))]
-            # temp = [word, tag, lesk(string, word)] # doesn't do POS tagging
+    normal_string=''
+    desired_tag=''
+    for word_and_tag in tagged_string:
+        word = word_and_tag.split('_')[0]
+        tag = word_and_tag.split('_')[1]
+        if word==desired_word:
+            desired_tag = tag
 
-        disambiguated.append(temp)
+        normal_string+=word+' '
 
-    return disambiguated
+    # ignore proper nouns and punctuation
+    if desired_tag=='NNP' or desired_tag=='NNPS' or desired_tag in string.punctuation:
+        return None
+
+    # if the POS can be resolved to a wordnet POS, call lesk with POS
+    # else call lesk without POS
+    wn_pos = ptb_to_wn_pos(desired_tag)
+    if wn_pos==None:
+        return lesk(normal_string,desired_word)
+    else:
+        return lesk(normal_string, desired_word, wn_pos)
+
+    # # tagged = pos_tag(word_tokenize(string.decode('ascii','ignore')))
+    # disambiguated = []
+    # for word_and_tag in string:
+    #     word, tag = word_and_tag.split('_')
+    #     print "word =",word
+    #     print "tag =",tag
+    #     # ignore proper nouns
+    #     if tag=='NNP' or tag=='NNPS':
+    #         temp=[word,tag,None]
+    #     else:
+    #         temp = [word, tag, lesk(string, word, ptb_to_wn_pos(tag))]
+    #         # temp = [word, tag, lesk(string, word)] # doesn't do POS tagging
+    #     disambiguated.append(temp)
+    # return disambiguated
 
 if __name__=='__main__':
-    print pos_tagger("John's big idea isn't all that bad.")
-    print pos_tagger("Ronald loves jelly beans more than Nancy.")
+    a= my_lesk("It_PRP was_VBD not_RB fear_NN or_CC dread_NN ._.".split(), 'fear')
+    b= my_lesk(["He_PRP" ,"should_MD","have_VB" ,"killed_VBN"], 'killed')
+    print a, a.definition()
+    print b, b.definition()
+    # print my_lesk("Ronald loves jelly beans more than Nancy.")
