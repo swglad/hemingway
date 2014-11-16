d8a4bb4550960322ad9a368d9fe0491b310d6669
Vikram Narayan <vnaray1@gmail.com>
2014-11-15 22:22:24 -0500

    had to remove some files from the commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from string import punctuation
 
 # CORPUS_FOLDER = "data/corpus"
-CORPUS_FOLDER = "corpus" # temporarily changed to facilitate lesk experiments
+CORPUS_FOLDER = "data/corpus_split" # temporarily changed to facilitate lesk experiments
 THESAURI_FOLDER = "data/thesaurus"
 MAPPING_FOLDER = "data/mapping"
 CORP_TAG = ".txt"
