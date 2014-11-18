6444b602c9fe4ff14bbe99634688ab1647d4673a
Vikram Narayan <vnaray1@gmail.com>
2014-11-17 22:00:57 -0500

    spaces

--- update_thesauri.py
+++ update_thesauri.py
@@ -33,7 +33,7 @@ def make_thesaurus(file_path):
 
             parsed = parse(line)
 
-            for i, tagged_word in enumerate(parsed.split()):
+            for tagged_word in parsed.split():
                 word = tagged_word.split('/')[0].strip().lower()
                 pos = tagged_word.split('/')[1][0] # get pos for word
 
@@ -109,7 +109,7 @@ def make_thesaurus_lesk(file_path):
                 continue
 
             # if word is verb, only add present tense to thesaurus
-            if tag[0] =='V': 
+            if tag[0] == 'V': 
                 if PRESENT in tenses(word.lower()):
                     thesaurus[str(synset)].update([word.lower()])
             elif tag[0] == 'N':
